27db5ac2132dbb41721e485fcfc501fb1c33cb3c
Kareem <kareem@Kareems-MacBook-Pro.local>
2022-11-27 15:59:54 +0300

    Completed Django Models CatCollector

--- Instructor-Sandbox/django-catcollector-project/catcollector/main_app/urls.py
+++ Instructor-Sandbox/django-catcollector-project/catcollector/main_app/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('about/', views.about, name='home'),
-    path('cats/', views.cats_index, name='index')
+    path('cats/', views.cats_index, name='index'),
+    # Cat Details Page 
+    path('cats/<int:cat_id>/', views.cats_detail, name='detail')
 ]
 
--- Instructor-Sandbox/django-catcollector-project/catcollector/main_app/views.py
+++ Instructor-Sandbox/django-catcollector-project/catcollector/main_app/views.py
@@ -1,21 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import Cat
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
 
-cats_list = [
-  Cat('Lolo', 'tabby', 'foul little demon', 3),
-  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+def cats_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+
 
 def cats_index(request):
+    cats_list = Cat.objects.all()
     return render(request, 'cats/index.html', {
         'cats': cats_list
     })
@@ -29,6 +25,6 @@ def about(request):
 def home(request):
     return render(request, 'home.html')
 
-    
+
 # def home(request):
 #     return HttpResponse('<h1>My First Django Route!</h1>')

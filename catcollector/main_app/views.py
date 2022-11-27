d6ffbff8a3a5e35f21659d2ec9de4fa22690c8d5
fjalafoo <fjalafoo@gmail.com>
2022-11-27 14:46:06 +0300

    updates

--- catcollector/main_app/views.py
+++ catcollector/main_app/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
+from .models import Cat
 
-class Cat:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
+# class Cat:
+#   def __init__(self, name, breed, description, age):
+#     self.name = name
+#     self.breed = breed
+#     self.description = description
+#     self.age = age
 
-cats = [
-  Cat('Lolo', 'tabby', 'foul little demon', 3),
-  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+# cats = [
+#   Cat('Lolo', 'tabby', 'foul little demon', 3),
+#   Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+#   Cat('Raven', 'black tripod', '3 legged cat', 4)
+# ]
+
+
 
 # View functions
 
@@ -22,4 +25,5 @@ def about(request):
     return render(request, 'about.html')
 
 def cats_index(request):
+ cats = Cat.objects.all()
   return render(request, 'cats/index.html', { 'cats': cats })

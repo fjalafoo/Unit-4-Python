fd502bdf5fbc2f0b2dfc4c68ea451c605c2d947a
fjalafoo <fjalafoo@gmail.com>
2022-11-28 13:40:37 +0300

    updates

--- catcollector/main_app/models.py
+++ catcollector/main_app/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+# A tuple of 2-tuples
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+
+
 # Create your models here.
 class Cat(models.Model):
     name = models.CharField(max_length=100)
@@ -10,3 +19,24 @@ class Cat(models.Model):
      # new code below
     def __str__(self):
         return self.name
+
+
+
+# Add new Feeding model below Cat model
+class Feeding(models.Model):
+ date = models.DateField()
+ meal = models.CharField(
+    max_length=1,
+    # add the 'choices' field option
+    choices=MEALS,
+    # set the default value for meal to be 'B'
+    default=MEALS[0][0]
+  )
+# Create a cat_id FK
+cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+def __str__(self):
+# Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
+
+
